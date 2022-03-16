8ba5eb7352356dd0a0fb9066e5428010a4238a31
gronnmann <gronnthecoder@gmail.com>
2022-03-16 18:16:35 +0100

    uke7oppg7

--- uke7/uke_07_oppg_7.py
+++ uke7/uke_07_oppg_7.py
@@ -1,12 +1,14 @@
 def line_best_fit(coords):
     split_coords = list(zip(*coords))
 
+    #print(f"Coords: {coords}, split: {split_coords}")
+
     split_x = split_coords[0]
     split_y = split_coords[1]
 
     n = len(split_x)
 
-    print(f"xCoords: {split_x}, yCoords: {split_y}, varNum: {n}")
+    #print(f"xCoords: {split_x}, yCoords: {split_y}, varNum: {n}")
 
     sum_x = float(sum(split_x))
     avg_x = sum_x / n
@@ -14,25 +16,36 @@ def line_best_fit(coords):
     sum_y = float(sum(split_y))
     avg_y = sum_y / n
 
-    print(f"Coords: {coords}, sumX: {sum_x}, avgX: {avg_x}, sumY: {sum_y}, avgY: {avg_y}")
+    #print(f"Coords: {coords}, sumX: {sum_x}, avgX: {avg_x}, sumY: {sum_y}, avgY: {avg_y}")
 
+    sigma_xy_packed = [c[0] * c[1] for c in coords]
+    sigma_xy = sum(sigma_xy_packed)
+    #print(f"Sigma xy: {sigma_xy_packed}, sum: {sigma_xy}")
 
-    mTop = sum_x * sum_y - ( (sum_x + sum_y) / (n)  )
-    mBottom = sum_x**2 - ( (sum_x**2) / n  )
+    sigma_x2 = sum(c**2 for c in split_x)
+
+    mTop = sigma_xy - ( (sum_x * sum_y) / (n)  )
+    mBottom = sigma_x2 - ( (sum_x**2) / n  )
 
     m = mTop / mBottom
 
-    print(f"mValues: mTop: {mTop}, mBot: {mBottom}, totalM: {m}")
+    #print(f"mValues: mTop: {mTop}, mBot: {mBottom}, totalM: {m}")
 
     b = avg_y - m * avg_x
 
-    print(f"b: {b}")
+    #print(f"b: {b}")
+
+    to_append = ""
+
+    if b > 0.0:
+        to_append = f"+ {b:.2f}"
+
+    return f"y = {m:.2f}x" + to_append
 
 
 coord_1 = ((1, 1), (2, 2.1), (3, 2.9))
-line_best_fit(coord_1)
+print(line_best_fit(coord_1))
 
-print("=========")
 
 coord_2 = ((0, 0), (1, 1), (2, 2), (3, 3))
-line_best_fit(coord_2)
+print(line_best_fit(coord_2))
